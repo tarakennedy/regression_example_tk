9fb4748c62d68fe2e0dc9b5cc519f6be0defaac0
tarakennedy <87974367+tarakennedy@users.noreply.github.com>
2024-02-15 13:18:22 +0000

    Update tests.py
    update

--- tests.py
+++ tests.py
@@ -1,8 +1,7 @@
-# tests.py
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-def test_regression():
+def predict_value(x):
     # Generate some sample data
     X = np.array([[1], [2], [3], [4], [5]])
     y = np.array([2, 4, 6, 8, 10])
@@ -14,8 +13,13 @@ def test_regression():
     model.fit(X, y)
 
     # Predict using the trained model
-    x_new = np.array([[6]])
-    y_pred = model.predict(x_new)
+    y_pred = model.predict(x)
 
-    # Check the predicted value
-    assert np.isclose(y_pred[0], 12.0), "Prediction error"
+    return y_pred
+
+# Test the regression prediction
+x_new = np.array([[6]])
+predicted_value = predict_value(x_new)
+expected_value = 15.0
+
+assert np.isclose(predicted_value[0], expected_value), "Prediction error"
